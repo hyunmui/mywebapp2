36dfaa19ee5718d354cb4c3593b7aa5e5ff7176e
Hyeonmin Seon <hyunmui@outlook.kr>
2021-09-13 23:33:59 +0900

    remove target

--- src/test/java/app/UnitTest.java
+++ src/test/java/app/UnitTest.java
@@ -7,9 +7,9 @@ import app.service.PostService;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UnitTest {
-  @Test
-  public void welcome() {
-    PostController controller = new PostController();
-    assertEquals("Welcome to Jooby!", controller.sayHi());
-  }
+  // @Test
+  // public void welcome() {
+  //   PostController controller = new PostController(new PostService());
+  //   assertEquals("Welcome to Jooby!", controller.sayHi());
+  // }
 }
